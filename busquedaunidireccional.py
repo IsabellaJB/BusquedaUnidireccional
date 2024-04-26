66639151fa8b66e4508ce6882ce0984be816cb5f
IsabellaJB <isabellajib5@gmail.com>
2024-04-25 18:11:22 -0600

    Update busquedaunidireccional.py

--- busquedaunidireccional.py
+++ busquedaunidireccional.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 def funcion_objetivo(x):
-    return x**2
+    # Función objetivo: (x1 - 10)^2 + (x2 - 10)^2
+    x1 = x[0]
+    x2 = x[1]
+    resultado = ((x1 - 10)**2) + ((x2 - 10)**2)
+    return resultado
 
 def busqueda_unidireccional(x_inicial, direccion, paso_maximo, tolerancia):
-    x_actual = x_inicial
+    x_actual = np.array(x_inicial) 
+    direccion = np.array(direccion) 
     while True:
         valor_actual = funcion_objetivo(x_actual)
         x_siguiente = x_actual + paso_maximo * direccion
@@ -14,11 +19,11 @@ def busqueda_unidireccional(x_inicial, direccion, paso_maximo, tolerancia):
         x_actual = x_siguiente
     return x_actual, funcion_objetivo(x_actual)
 
-x_inicial = 3.0
-direccion = 1.0
+
+x_inicial = [3.0, 3.0]
+direccion = [1.0, 1.0]
 paso_maximo = 0.1
 tolerancia = 1e-6
-
 resultado, valor_minimo = busqueda_unidireccional(x_inicial, direccion, paso_maximo, tolerancia)
 
 print("El mínimo de la función se encuentra en x =", resultado)
