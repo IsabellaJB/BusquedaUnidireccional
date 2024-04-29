a7ea526559275a933624ebc3ee35188cfe5dc69a
IsabellaJB <isabellajib5@gmail.com>
2024-04-29 09:12:36 -0600

    HOlaaaa

--- busquedaunidireccional.py
+++ busquedaunidireccional.py
@@ -1,30 +1,33 @@
 import numpy as np
 
 def funcion_objetivo(x):
-    # Función objetivo: (x1 - 10)^2 + (x2 - 10)^2
     x1 = x[0]
     x2 = x[1]
     resultado = ((x1 - 10)**2) + ((x2 - 10)**2)
     return resultado
 
-def busqueda_unidireccional(x_inicial, direccion, paso_maximo, tolerancia):
-    x_actual = np.array(x_inicial) 
-    direccion = np.array(direccion) 
-    while True:
-        valor_actual = funcion_objetivo(x_actual)
-        x_siguiente = x_actual + paso_maximo * direccion
-        valor_siguiente = funcion_objetivo(x_siguiente)
-        if np.abs(valor_siguiente - valor_actual) < tolerancia:
-            break
-        x_actual = x_siguiente
-    return x_actual, funcion_objetivo(x_actual)
+def busqueda_unidireccional(punto_inicial, direccion_busqueda, funcion_objetivo, paso_alpha=0.01):
+    def evaluar_objetivo_en_alpha(alpha):
+        x_alpha = punto_inicial + alpha * direccion_busqueda
+        return funcion_objetivo(x_alpha)
+    
+    valor_minimo = float('inf')
+    punto_minimo = None
+    alpha = 0.0
+    while alpha <= 1.0:
+        valor_actual = evaluar_objetivo_en_alpha(alpha)
+        if valor_actual < valor_minimo:
+            valor_minimo = valor_actual
+            punto_minimo = punto_inicial + alpha * direccion_busqueda
+        alpha += paso_alpha
+    return punto_minimo, valor_minimo
 
+# Definimos un punto inicial y una dirección de búsqueda
+punto_inicial = np.array([0, 0])
+direccion_busqueda = np.array([1, 1])
 
-x_inicial = [3.0, 3.0]
-direccion = [1.0, 1.0]
-paso_maximo = 0.1
-tolerancia = 1e-6
-resultado, valor_minimo = busqueda_unidireccional(x_inicial, direccion, paso_maximo, tolerancia)
+# Aplicamos la búsqueda unidireccional
+punto_minimo, valor_minimo = busqueda_unidireccional(punto_inicial, direccion_busqueda, funcion_objetivo)
 
-print("El mínimo de la función se encuentra en x =", resultado)
-print("El valor mínimo de la función es f(x) =", valor_minimo)
+print("Punto mínimo:", punto_minimo)
+print("Valor mínimo:", valor_minimo)
